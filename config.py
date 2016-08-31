465c36fddb0727622a901ead3a0c936aa13b56ce
Ojas Ahuja <OjasA@users.noreply.github.com>
2016-08-31 18:03:00 -0500

    Disable CSRF_TOKEN in config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+WTF_CSRF_ENABLED = False
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
