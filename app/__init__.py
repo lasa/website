476233d188e445f614eea053c258b7bb68f60a30
Ojas Ahuja <oja@users.noreply.github.com>
2016-09-03 11:21:51 -0500

    Initial LoginManager setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 #!../flask/bin/python
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from app import views, models, login_signup
--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
-from app import app
+from app import app, login_manager
 from flask import render_template
+from flask.ext.login import login_user, logout_user, current_user, \
+    login_required
+
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/history/')
 def history():
-	return render_template("history.html")
+    return render_template("history.html")
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
