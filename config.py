52e40b278dcdf824bc347d01f5a1eb769c0feaab
Ojas Ahuja <ojas@ahujas.org>
2016-09-04 15:01:47 -0500

    Make Flask-Login use the same secret key as WTForms

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
-secret_key = "lol"
+SECRET_KEY = "lol"
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
