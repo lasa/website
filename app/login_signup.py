70c7950f2edc7c6f510d73c6ce888b4d118767ac
Ojas Ahuja <oja@users.noreply.github.com>
2016-09-04 15:20:59 -0500

    Clear candidate passwords from memory using ctypes
    Without the clearing of candidate passwords, they remain in memory even
    after all operations utilizing them have completed. This allows an
    attacker with access to the memory or swapfile of the host machine to
    view the candidate passwords in plaintext.
    
    Python doesn't allow for the sufficient level of low level control to
    manage the manipulation of strings in memory, so the clearing is
    achieved through libc.
    
    A writeup about this is located at
    https://web.archive.org/web/20100929111257/http://www.codexon.com/posts/clearing-passwords-in-memory-with-python.

--- app/login_signup.py
+++ app/login_signup.py
@@ -8,8 +8,10 @@ from flask_wtf import Form
 from wtforms import validators, StringField, PasswordField
 from wtforms.validators import DataRequired
 import bcrypt, hmac
+import sys
+import ctypes
 
-    
+
 class LoginForm(Form):
     username = StringField('Username:', validators=[validators.DataRequired(), validators.Length(min=4,max=16)])
     password = PasswordField('Password:', [
@@ -22,11 +24,21 @@ def generate_hash(password):
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed
 
+
 def check_password(candidate_password, pwhash):
     if (hmac.compare_digest(bcrypt.hashpw(candidate_password.encode("utf-8"), pwhash), pwhash)):
         return True
     else:
         return False
+
+
+def overwrite_password(string):
+    location = id(string) + 20
+    size = sys.getsizeof(string) - 20
+    # change the "6" to whatever is on the actual system
+    memset = ctypes.CDLL("libc.so.6").memset
+    print("Clearing 0x%08x size %i bytes" % (location, size))
+    memset(location, 0, size)
 
 
 def login():
@@ -39,15 +51,22 @@ def login():
         real_user = User.query.filter_by(name=candidate_username).first()
         if real_user is None:
             form.username.errors.append("Username does not exist.")
+            overwrite_password(candidate_password)
+            del candidate_password
             return render_template("login.html", form=form)
         else:
             if check_password(candidate_password, real_user.password):
                 login_user(User.query.get(real_user.id))
+                overwrite_password(candidate_password)
+                del candidate_password
                 return redirect("/")
             else:
                 form.password.errors.append("Username and password do not match.")
+                overwrite_password(candidate_password)
+                del candidate_password
                 return render_template("login.html", form=form)
     return render_template("login.html", form=form)
+
 
 def logout():
     logout_user()
