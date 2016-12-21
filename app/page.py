efc03699126178e6bd64c7dd99a4a3b94eb5c22e
kevin3-black <kevin3.black@gmail.com>
2016-12-20 18:14:55 -0600

    Allow editing of page order and divider placement

--- app/page.py
+++ app/page.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, request
 from flask_login import current_user
 from flask_wtf import Form
 from wtforms import validators, StringField, TextAreaField, HiddenField, SelectField, BooleanField
+from flask_wtf.html5 import IntegerField
 from wtforms.validators import DataRequired
 import datetime, time
 
@@ -20,11 +21,15 @@ def TinyMCE(field):
          <script>tinymce.init({ 
             selector:'#editor', 
             theme: 'modern',
+            height: 800,
 			plugins: [
             'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
             'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-            'save table contextmenu directionality emoticons template paste textcolor autoresize'
+            'save table contextmenu directionality emoticons template paste textcolor'
             ],
+            table_default_attributes: {
+            class: 'table-condensed'
+            },
             content_css: '/static/css/tinymce.css',
             toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor'
          });</script>
@@ -33,6 +38,8 @@ def TinyMCE(field):
 class NewPageForm(Form):
     title = StringField('Title:', validators=[validators.Length(min=0,max=1000)])
     category = SelectField('Category:', choices=choices)
+    dividerBelow = BooleanField('Divider below page name in dropdown menu')
+    index = IntegerField('Ordering index (lower is higher up):')
     body = TextAreaField('Body:', validators=[validators.Length(min=0,max=75000)], widget=TinyMCE)
     bodyhtml = HiddenField()
 
@@ -43,15 +50,21 @@ def new_page():
         title = form.title.data
         body = form.bodyhtml.data
         category = form.category.data
+        dividerBelow = form.dividerBelow.data
+        index = form.index.data
 
         if len(title) < 1:
             form.title.errors.append("This field is required.")
             form.body.data = body
-            return views.render_with_navbar("editpage.html", form=form, title=title)
+            return views.render_with_navbar("newpage.html", form=form, title=title, index=index)
+        if index<0 or index>100:
+            form.index.errors.append("Number must be between 0 and 100.")
+            form.body.data = body
+            return views.render_with_navbar("newpage.html", form=form, title=title, index=index)
 
         name = "-".join(title.split(" ")).lower()
 
-        newpage = Page(title=title, name=name, category=category, body=body)
+        newpage = Page(title=title, name=name, category=category, dividerBelow=dividerBelow, index=index, body=body)
         db.session.add(newpage)
         db.session.commit()
         time.sleep(0.5);
@@ -72,8 +85,10 @@ def edit_page(page_name):
     title = currentPage.title
     bodyhtml = currentPage.body
     category = currentPage.category
+    dividerBelow = currentPage.dividerBelow
+    index = currentPage.index
 
-    form = NewPageForm(category=category)
+    form = NewPageForm(category=category, dividerBelow=dividerBelow)
 
     form.body.data = bodyhtml
 
@@ -81,11 +96,18 @@ def edit_page(page_name):
         newtitle = form.title.data
         newbody = form.bodyhtml.data
         newcategory = form.category.data
+        newdividerBelow = form.dividerBelow.data
+        newindex = form.index.data
 
         if len(newtitle) < 1:
             form.title.errors.append("This field is required.")
             form.body.data = newbody
-            return views.render_with_navbar("editpage.html", form=form, title=newtitle)
+            return views.render_with_navbar("editpage.html", form=form, title=newtitle, index=newindex)
+
+        if index<0 or index>100:
+            form.index.erros.append("Number must be between 0 and 100.")
+            form.body.data = newbody
+            return views.render_with_navbar("editpage.html", form=form, title=newtitle, index=newindex)
 
         newname = "-".join(newtitle.split(" ")).lower()
 
@@ -94,17 +116,19 @@ def edit_page(page_name):
             if page:
                 form.title.errors.append("A page with this name already exists.")
                 form.body.data = newbody
-                return views.render_with_navbar("editpage.html", form=form, title=newtitle)
+                return views.render_with_navbar("editpage.html", form=form, title=newtitle, index=newindex)
 
         currentPage.title = newtitle
         currentPage.body = newbody
         currentPage.name = newname
         currentPage.category = newcategory
+        currentPage.dividerBelow = newdividerBelow
+        currentPage.index = newindex
         db.session.commit()
         time.sleep(0.5)
         return redirect("/page/" + newname)
 
-    return views.render_with_navbar("editpage.html", form=form, title=title)
+    return views.render_with_navbar("editpage.html", form=form, title=title, index=index)
 
 
 def delete_page(page_name):
--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from flask_login import login_required
 from flask import render_template, request, redirect
 
 def render_with_navbar(template, **kwargs):
-    pages = {'calendars': Page.query.filter_by(category='calendars'),
-                'about': Page.query.filter_by(category='about'),
-                'academics': Page.query.filter_by(category='academics'),
-                'students': Page.query.filter_by(category='students'),
-                'parents': Page.query.filter_by(category='parents'),
-                'admissions': Page.query.filter_by(category='admissions')}
+    pages = {'calendars': Page.query.filter_by(category='calendars').order_by(Page.index.asc()),
+                'about': Page.query.filter_by(category='about').order_by(Page.index.asc()),
+                'academics': Page.query.filter_by(category='academics').order_by(Page.index.asc()),
+                'students': Page.query.filter_by(category='students').order_by(Page.index.asc()),
+                'parents': Page.query.filter_by(category='parents').order_by(Page.index.asc()),
+                'admissions': Page.query.filter_by(category='admissions').order_by(Page.index.asc())}
     return render_template(template, pages=pages, **kwargs)
 
 @app.route('/')
