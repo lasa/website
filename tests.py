c4f8fe633a534ad032092044752f03b16e21debb
Ojas Ahuja <oja@users.noreply.github.com>
2016-12-28 03:47:42 -0600

    Add inital testing framework (#3)
    * Add inital test
    
    * Modify unit test to work with MariaDB

---
+++ tests.py
@@ -0,0 +1,31 @@
+#!flask/bin/python
+import os
+import unittest
+import bcrypt
+import hmac
+
+from config import basedir
+from app import app, db
+from app.models import User
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/website-testdb'
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_signup(self):
+        u = User(name="harambe", email="harambe@cincinnatizoo.org",
+                 password=bcrypt.hashpw("IAmHarambe".encode("utf-8"), bcrypt.gensalt2()))
+        db.session.add(u)
+        db.session.commit()
+
+if __name__ == '__main__':
+    unittest.main()
