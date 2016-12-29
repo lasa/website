f2b7637e32b7de962b99ae97146e078c592379c9
kevin3-black <kevin3.black@gmail.com>
2016-12-29 14:51:46 -0600

    Add list of images from uploads to TinyMCE editor

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 from app import app
 from app.models import Page
 from flask import render_template
+import os
 
 def render_with_navbar(template, **kwargs):
     pages = {'calendars': Page.query.filter_by(category='calendars').order_by(Page.index.asc()),
@@ -13,11 +14,20 @@ def render_with_navbar(template, **kwargs):
 
 #custom widget for rendering a TinyMCE input
 def TinyMCE(field):
+    uploads = os.listdir(os.path.join('app', app.config['UPLOAD_FOLDER']))
+    liststr = "["
+    image_extensions = ["png", "jpg", "jpeg", "gif", "bmp"]
+    for upload in uploads:
+        if '.' in upload and upload.rsplit('.', 1)[1].lower() in image_extensions:
+            liststr += "{title: '" + upload + "', value: '/uploads/" + upload + "'},"
+    liststr = liststr[:-1] + ']'
+
     return """  <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
          <script>tinymce.init({ 
             selector:'#editor', 
             theme: 'modern',
             height: 800,
+            convert_urls: false,
             fontsize_formats: '8pt 10pt 11pt 12pt 14pt 18pt 24pt 36pt',
             setup: function(ed) {
                      ed.on('init', function(ed) {
@@ -33,7 +43,8 @@ def TinyMCE(field):
             class: 'table-condensed'
             },
             content_css: '/static/css/tinymce.css',
-            toolbar: 'styleselect | fontsizeselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | forecolor backcolor'
+            toolbar: 'styleselect | fontsizeselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | forecolor backcolor',
+            image_list: """ + liststr + """
          });
          </script>
          <textarea id='editor'> %s </textarea>""" % field._value()
