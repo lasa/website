0f7f744ca90af7b23bf47719c7da577ce5290ec3
kevin3-black <kevin3.black@gmail.com>
2017-04-26 19:22:34 -0500

    Switch news and messages to TinyMCE/HTMl bodies

--- app/post.py
+++ app/post.py
@@ -3,21 +3,22 @@ from app.models import User, Post, Message
 from flask import Flask, redirect, request
 from flask_login import current_user
 from flask_wtf import Form
-from wtforms import validators, StringField, TextAreaField
+from wtforms import validators, StringField, TextAreaField, HiddenField
 from wtforms.validators import DataRequired
 import time, datetime
 
 
 class NewPostForm(Form):
     title = StringField('Title:', validators=[validators.DataRequired(), validators.Length(min=0,max=1000)])
-    body = TextAreaField('Body:')
+    body = TextAreaField('Body:', validators=[validators.Length(min=0,max=75000)], widget=utils.TinyMCE)
+    bodyhtml = HiddenField()
 
 
 def new_post():
     form = NewPostForm()
     if form.validate_on_submit():
         title = form.title.data
-        body = form.body.data
+        body = form.bodyhtml.data
 
         newpost = Post(title=title, body=body, author=current_user.id, timestamp=datetime.datetime.now())
         db.session.add(newpost)
@@ -31,7 +32,7 @@ def new_message():
     form = NewPostForm()
     if form.validate_on_submit():
         title = form.title.data
-        body = form.body.data
+        body = form.bodyhtml.data
 
         newpost = Message(title=title, body=body, author=current_user.id, timestamp=datetime.datetime.now())
         db.session.add(newpost)
@@ -56,17 +57,18 @@ def edit_post():
 
     title = currentPost.title
     body = currentPost.body
+    form.body.data = body
 
     if form.validate_on_submit():
         newtitle = form.title.data
-        newbody = form.body.data
+        newbody = form.bodyhtml.data
         currentPost.title = newtitle
         currentPost.body = newbody
         db.session.commit()
         time.sleep(0.5)
         return redirect("/news?postid="+postid)
 
-    return utils.render_with_navbar("news/editpost.html", form=form, title=title, body=body, heading="News Item")
+    return utils.render_with_navbar("news/editpost.html", form=form, title=title, heading="News Item")
 
 def edit_message():
     postid = request.args.get("postid")
@@ -81,17 +83,18 @@ def edit_message():
 
     title = currentPost.title
     body = currentPost.body
+    form.body.data = body
 
     if form.validate_on_submit():
         newtitle = form.title.data
-        newbody = form.body.data
+        newbody = form.bodyhtml.data
         currentPost.title = newtitle
         currentPost.body = newbody
         db.session.commit()
         time.sleep(0.5)
         return redirect("/messages?postid="+postid)
 
-    return utils.render_with_navbar("news/editpost.html", form=form, title=title, body=body, heading="Principal's Message")
+    return utils.render_with_navbar("news/editpost.html", form=form, title=title, heading="Principal's Message")
 
 
 def delete_post():
